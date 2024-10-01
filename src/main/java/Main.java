4c27d448dcda6df18eaec1245bc95144d5a452b9
domshi <90562421+domshi@users.noreply.github.com>
2024-10-01 22:46:20 +0000

    End class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,22 @@ public class Main {
   
   public static void main(String[] args) {//start main
 
+    //special addition
+    String a = "How are you today?";
+    String b = "\nI haven't seen you in a while.";
+    System.out.println(a + b);
+
+    double rando;
+    rando = Math.random();
+    rando = rando * 10;
+    int rando2 = (int)rando;
+    System.out.println(rando2);
+    
     int value = 1;
     int quest = 3;
+
     
-    switch (value) {//start switch
+    switch (rando2) {//start switch
       case 1:
         if (quest > 3) {
           System.out.println("Special quest complete");
@@ -52,3 +64,4 @@ public class Main {
     
   }
 }
+
